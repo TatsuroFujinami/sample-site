deac5701b0963b1fc55339e3e2ecb0d370e44679
TatsuroFujinami <79361770+TatsuroFujinami@users.noreply.github.com>
2021-08-15 13:22:15 +0900

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,13 @@
+'use strict';
+
+{
+    $(function () {
+        $('.bxslider').bxSlider({
+            auto: true,
+            mode: 'fade',
+            speed: 1000,
+            pager: true,
+            controls: false,
+        });
+    });
+}
